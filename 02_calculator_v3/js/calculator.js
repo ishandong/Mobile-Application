0d7e2e969f2c33433dedb1c17fadf4841a767129
ishandong <ishandong12@gmail.com>
2026-02-04 11:32:49 +0900

    Add files via upload

---
+++ 02_calculator_v3/js/calculator.js
@@ -0,0 +1,44 @@
+'use strict';
+const elementSelect = document.getElementById("calcType");
+const elementNum1 = document.getElementById("num1");
+const elementNum2 = document.getElementById("num2");
+const elementResult = document.getElementById("result");
+const elementbtnEqual = document.getElementById ("btnEqual");
+
+elementSelect.addEventListener("change",clear);
+elementNum1.addEventListener("change",clear);
+elementNum2.addEventListener("change",clear);
+
+elementbtnEqual.addEventListener("click",update)
+    /** 計算します。 */
+    function update() {
+        const result = calculate(
+            Number(elementNum1.value),
+            Number(elementNum2.value),
+            elementSelect.value
+        );
+        elementResult.innerHTML = result;
+    }
+
+        // 計算の種類で処理を分岐
+        function calculate(num1,num2,calcType){
+            let result;
+        switch (calcType) {
+            case "type-add": // 足し算の場合
+                result = num1 + num2;
+                break;
+            case "type-substract": // 引き算の場合
+                result = num1 - num2;
+                break;
+            case "type-multiply": // 掛け算の場合
+                result = num1 * num2;
+                break;
+            case "type-divide": // 割り算の場合
+                result = num1 / num2;
+                break;
+        }
+        return result;
+    }
+    function clear(){
+        elementResult.innerHTML = "";
+    }
